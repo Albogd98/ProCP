a0fb647dac0c43aca4b925b6e52239f12fbe22b9
unknown <jmichael1303@gmail.com>
2019-04-13 08:15:36 -0700

    first scene added

---
+++ Traffic Simulation/Assets/Car.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+
+    private float _speed = 10;
+    private float _acceleration = 1;
+    
+
+
+    public void Accelerate()
+    {
+        if (_speed < 5)
+        {
+            _speed += _acceleration;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveForward();
+    }
+    void moveForward()
+    {
+        RaycastHit hit = new RaycastHit();
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+
+            if (hit.distance > 2f)
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                transform.Translate(0, 0, _speed * Time.deltaTime);
+            }
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+        }
+
+
+       
+    }
+
+}
